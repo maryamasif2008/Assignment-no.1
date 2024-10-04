a678f4bcb3da26408ab3de009b2f3fc9d2ef647c
maryamasif2008 <marayam.asif123@gmail.com>
2024-10-04 18:36:53 +0500

    added files index.html and js

---
+++ Assignment no .01/index.js
@@ -0,0 +1,41 @@
+                                          // CHAPTER 01  
+
+                                            /* Question 01 */
+alert("This is an alert message!");        
+
+                                           /* Question 02 */
+alert("Welcome to my website!"); 
+
+                                           /* Question 03 */
+ Uncaught ReferenceError: Alert is not defined                                           
+
+                                           /* Question 04 */
+alert("Hello, world!");  
+
+                                           /* Question 05 */
+alert("This is an alert message.");                                           
+
+                                            
+                                         // CHAPTER 02
+
+                                           /* Question o1 */
+var message = "Hello, world!";
+alert(message);
+
+                                           /* Question 02 */
+var age;
+age = 25;
+
+alert(age);    
+
+                                           /* Question 03 */
+var greeting = "Good morning!";
+alert(greeting);
+
+greeting = "Good evening!";
+alert(greeting);           
+
+                                           /* Question 04 */
+var favoriteColor = "blue";
+alert("My favorite color is " + favoriteColor);  
+                                           
